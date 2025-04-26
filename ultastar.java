e547d14fa362225fc3244ea4505d87811ee87c08
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2025-04-26 21:52:36 +0530

    inverse piramid

---
+++ ultastar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class ultastar
+{
+    public static void main(String[] arg)
+    {
+        int i=5,j;
+        for(i=5;i>=0;i--)
+        {
+            for (j=0;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
