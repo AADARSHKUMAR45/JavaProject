6d54a7879e67d73a50980e457d6ca79f676b7ea4
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-01-16 18:01:00 +0530

    O(n)

---
+++ kadanalgo.java
@@ -0,0 +1,19 @@
+public class kadanalgo {
+     static void kadan(int nums[]){
+        int ms= Integer.MIN_VALUE;
+        int cs=0;
+        for(int i=0;i<nums.length;i++){
+            cs=cs+nums[i];
+            if(cs<0){
+                cs=0;
+            }
+            ms=Math.max(ms,cs);
+        }
+        System.out.println(ms);
+    }
+
+    public static void main(String[] args){
+        int nums[]={-2,-3,4,-1,-2,1,5,-3};
+        kadan(nums);
+    }
+}
