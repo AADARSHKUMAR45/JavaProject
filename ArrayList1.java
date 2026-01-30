e22ba02d41d12fa1dd3b2a7680dacb032d2be966
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-01-30 19:53:12 +0530

    Add files via upload

---
+++ ArrayList1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class ArrayList1 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Mango");
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+
+        System.out.println("ArrayList: " + list);
+    }
+}
