7f99a15c1572e19d64644f90e42643c3e869131e
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-01-31 19:46:26 +0530

    reverse string

---
+++ reverseString.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class reverseString {
+    static void reverse(char[] s){
+        int left=0,right=s.length-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        char[] s={'h','e','l','l','o'};
+        reverse(s);
+        System.out.println("After Reverse:  " + Arrays.toString(s));
+
+    }
+}
