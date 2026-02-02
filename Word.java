111cf6bc98622390ef0762d5ffa8417c89e3087d
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-02-02 22:27:41 +0530

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,14 @@
+public class Word {
+    static void Longest(String str) {
+        int n=str.length();
+        String longest="";
+        for(int i=0;i<n;i++){
+
+        }
+
+    }
+    public static void main(String[] args) {
+        String str="hello world";
+
+    }
+}
